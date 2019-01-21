5ce7c41084938a46c7658396a71eedb3ec31e7c0
Shreyas S Bhat <35568964+shreyassbhat@users.noreply.github.com>
2019-01-21 17:35:46 +0530

    Add files via upload

---
+++ WriteToFile/ClientApp/src/components/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export class Counter extends Component {
+  displayName = Counter.name
+
+  constructor(props) {
+    super(props);
+    this.state = { currentCount: 0 };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  incrementCounter() {
+      fetch('api/Write', {
+          method: 'POST'
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a React component.</p>
+
+        <p>Current count: <strong>{this.state.currentCount}</strong></p>
+
+        <button onClick={this.incrementCounter}>Increment</button>
+      </div>
+    );
+  }
+}
---
+++ WriteToFile/ClientApp/src/components/FetchData.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export class FetchData extends Component {
+  displayName = FetchData.name
+
+  constructor(props) {
+    super(props);
+    this.state = { forecasts: [], loading: true };
+
+    fetch('api/Write')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ forecasts: data, loading: false });
+      });
+  }
+
+  static renderForecastsTable(forecasts) {
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>NAME</th>
+           
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map(forecast =>
+            <tr key={forecast.dateFormatted}>
+              <td>{forecast}</td>
+              
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : FetchData.renderForecastsTable(this.state.forecasts);
+
+    return (
+      <div>
+        <h1>Weather forecast</h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+}
